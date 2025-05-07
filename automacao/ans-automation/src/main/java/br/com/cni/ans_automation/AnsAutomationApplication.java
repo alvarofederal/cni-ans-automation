39fa6f0e6ad7f5cd01b6ffb44bb8e755a1fa1aac
AlvaroPGomes <alvaropgomes@gmail.com>
2025-05-06 22:18:29 -0300

    chore: criando projeto de automação java e projeto power bi

---
+++ automacao/ans-automation/src/main/java/br/com/cni/ans_automation/AnsAutomationApplication.java
@@ -0,0 +1,13 @@
+package br.com.cni.ans_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsAutomationApplication.class, args);
+	}
+
+}
---
+++ automacao/ans-automation/src/test/java/br/com/cni/ans_automation/AnsAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cni.ans_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
